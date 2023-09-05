63781087ebdeb868831c6eb1e910dd6e44b85222
송현주 <shyunju7@naver.com>
2023-09-05 17:14:05 +0900

    add : sass 폴더 구조화 및 scss 적용 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
+import Image from "./assets/img-mong.png";
 const App = () => {
-  return <h1>Hello!</h1>;
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <img src={Image} alt="몽이 이미지" width="200" />
+    </div>
+  );
 };
 export default App;
