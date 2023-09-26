2af9cb7ed752ff649249b1ab5963785ac104d541
송현주 <shyunju7@naver.com>
2023-09-26 20:38:06 +0900

    feat : homeApi test & chpater state 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { useEffect, useState } from "react";
 import Select from "./components/Select";
 import Sidebar from "./components/Sidebar";
+import { homeApi } from "./apis";
 export interface Option {
   text: string;
   value: string;
+}
+export interface Chapter {
+  id: number;
+  name: string;
+  parentId: number;
+  subChapter: Chapter[];
 }
 const data: Option[] = [
   {
@@ -19,9 +27,27 @@ const data: Option[] = [
   },
 ];
 const App = () => {
+  const [isOpenSidebar, setOpenSidebar] = useState<boolean>(false);
+  const [chapters, setChapters] = useState<Chapter[]>([]);
+  const handleGetChapters = async () => {
+    try {
+      const { data } = await homeApi.getAllChapters();
+      setChapters(data);
+    } catch (error) {
+      console.log("handleGetChapters()...", error);
+    }
+  };
+  useEffect(() => {
+    handleGetChapters();
+  }, []);
   return (
     <div id="app-container">
-      <Sidebar />
+      {isOpenSidebar ? (
+        <Sidebar chapters={chapters} />
+      ) : (
+        <button onClick={() => setOpenSidebar((prev) => !prev)}>버튼</button>
+      )}
+
       <main>
         <Select data={data} />
       </main>
