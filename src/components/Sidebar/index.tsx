3b3da9c582b4b75b8f2453586e6001bfa5185b1e
송현주 <shyunju7@naver.com>
2023-10-22 15:40:57 +0900

    style : a태그 스타일 적용 & 사이드바 api 연결 및 동적 처리

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import "./sidebar.scss";
 import { Chapter } from "@src/types/chapter";
+import { useNavigate } from "react-router-dom";
 
 interface SidebarProps {
   chapters: Chapter[];
 }
 const Sidebar = ({ chapters }: SidebarProps) => {
+  const navigate = useNavigate();
   const handleOnClickNavTitle = (navTitle: string) => {
     const subNavName = document.querySelector<HTMLElement>(`#${navTitle}`);
     const subNavInner = document.querySelector<HTMLElement>(
@@ -33,31 +35,44 @@ const Sidebar = ({ chapters }: SidebarProps) => {
         </span>
         <span>Intro</span>
       </button>
-      <button onClick={() => handleOnClickNavTitle("components")}>
-        <span aria-label="icon" role="img"></span>
-        <span>Components</span>
-        <span>
-          <svg
-            width="12"
-            height="9"
-            viewBox="0 0 16 9"
-            fill="#ffffff"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M0.340457 0.340457C0.7944 -0.113486 1.53039 -0.113486 1.98433 0.340457L7.85495 6.21108C7.93506 6.29119 8.06494 6.29119 8.14505 6.21108L14.0157 0.340457C14.4696 -0.113486 15.2056 -0.113486 15.6595 0.340457C16.1135 0.7944 16.1135 1.53039 15.6595 1.98433L9.78892 7.85495C8.80092 8.84295 7.19907 8.84294 6.21108 7.85495L0.340457 1.98433C-0.113486 1.53039 -0.113486 0.7944 0.340457 0.340457Z"
-            />
-          </svg>
-        </span>
-      </button>
-      <div id="components" className="subnav">
-        <div className="subnav-inner">
-          <button>select</button>
-          <button>input</button>
-        </div>
-      </div>
+
+      {chapters &&
+        chapters.map((chapter) => (
+          <div key={chapter.id}>
+            <button onClick={() => handleOnClickNavTitle(`${chapter.name}`)}>
+              <span aria-label="icon" role="img"></span>
+              <span>{chapter.name}</span>
+              <span>
+                <svg
+                  width="12"
+                  height="9"
+                  viewBox="0 0 16 9"
+                  fill="#ffffff"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M0.340457 0.340457C0.7944 -0.113486 1.53039 -0.113486 1.98433 0.340457L7.85495 6.21108C7.93506 6.29119 8.06494 6.29119 8.14505 6.21108L14.0157 0.340457C14.4696 -0.113486 15.2056 -0.113486 15.6595 0.340457C16.1135 0.7944 16.1135 1.53039 15.6595 1.98433L9.78892 7.85495C8.80092 8.84295 7.19907 8.84294 6.21108 7.85495L0.340457 1.98433C-0.113486 1.53039 -0.113486 0.7944 0.340457 0.340457Z"
+                  />
+                </svg>
+              </span>
+            </button>
+            <div id={chapter.name} className="subnav">
+              <div className="subnav-inner">
+                {chapter.subChapter.length > 0 &&
+                  chapter.subChapter.map((sub) => (
+                    <button
+                      key={sub.id}
+                      onClick={() => navigate(`chapters/${sub.name}`)}
+                    >
+                      {sub.name}
+                    </button>
+                  ))}
+              </div>
+            </div>
+          </div>
+        ))}
     </aside>
   );
 };
